55570507deb311fc360040f95ccd687376d06ee8
Sanjana <53556890+Sanjana8055@users.noreply.github.com>
2023-01-18 12:28:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from jinja2.utils import markupsafe
+
 app = Flask(__name__)
 
-@app.route("/")
-def helloworld():
+@app.route('/')
+def response():
     return "hello world!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True) 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80) 
